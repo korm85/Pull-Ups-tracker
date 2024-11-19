f3c6e6e5255dc3d4ae4c33cd43ace94184f078ee
korm85 <korm85@gmail.com>
2024-11-19 23:31:27 +0200

    Resolve merge conflicts and fix app appearance

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const CACHE_NAME = 'workout-tracker-cache-v1';
 const urlsToCache = [
     '/',
@@ -19,7 +18,7 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => { Jacob
+            .then(response => {
                 if (response) {
                     return response;
                 }
@@ -27,33 +26,3 @@ self.addEventListener('fetch', event => {
             })
     );
 });
-=======
-const CACHE_NAME = 'workout-tracker-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => { Jacob
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
->>>>>>> 820771c9634541779e8cb30501a1a1a25bb1446f
